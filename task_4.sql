caa752c974a4c82989939854fe609f9ddc8e320f
chris <127218220+chrisisaboke01@users.noreply.github.com>
2025-01-28 12:31:35 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -2,6 +2,6 @@
 USE alx_book_store;
 
 -- Query to get the full description of the books table
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
-FROM information_schema.COLUMNS
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
