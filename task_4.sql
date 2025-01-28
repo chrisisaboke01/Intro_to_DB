5f91f74e721ac238db85c7bbcb2b17e44869f538
chris <127218220+chrisisaboke01@users.noreply.github.com>
2025-01-28 12:34:44 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,18 @@
--- Select the database passed as an argument
-USE alx_book_store;
-
--- Query to get the full description of the books table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+-- task_4.sql
+SELECT 
+    COLUMN_NAME AS `Field`,
+    CONCAT(DATA_TYPE,
+        IF(CHARACTER_MAXIMUM_LENGTH IS NOT NULL, 
+            CONCAT('(', CHARACTER_MAXIMUM_LENGTH, ')'), 
+            IF(DATA_TYPE = 'decimal', 
+                CONCAT('(', NUMERIC_PRECISION, ',', NUMERIC_SCALE, ')'), 
+                '')
+        )
+    ) AS `Type`,
+    IS_NULLABLE AS `Null`,
+    COLUMN_KEY AS `Key`,
+    COLUMN_DEFAULT AS `Default`,
+    EXTRA AS `Extra`
+FROM information_schema.columns
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'Books'
+ORDER BY ORDINAL_POSITION;
